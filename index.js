ab2f9be5e7274b4f77d8dee2d0ced0034ede7bbb
VaibhavShukla4 <96537580+VaibhavShukla4@users.noreply.github.com>
2023-05-07 11:13:40 +0530

    delete-product-api-&-json-web-token impliment

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const User = require("./db/User");
 const Products = require("./db/Products");
 const app = express();
 // app.use is middleware
+const Jwt = require("jsonwebtoken");
+const jwtKey = "e-comm";
 app.use(express.json());
 app.use(cors());
 
@@ -17,7 +19,15 @@ app.post("/register", async (req, res) => {
   result = result.toObject();
   // console.log(result);
   delete result.password;
-  res.send(result);
+  Jwt.sign({ result }, jwtKey, { expiresIn: "2h" }, (err, token) => {
+    console.log(token);
+    if (err) {
+      res.send({
+        result: "something went wrong , Please try after sometime",
+      });
+    }
+    res.send({ result, auth: token });
+  });
 });
 // for login
 app.post("/login", async (req, res) => {
@@ -25,7 +35,15 @@ app.post("/login", async (req, res) => {
   let user = await User.findOne(req.body).select("-password");
   if (req.body.email && req.body.password) {
     if (user) {
-      res.send(user);
+      Jwt.sign({ user }, jwtKey, { expiresIn: "2h" }, (err, token) => {
+        console.log(token);
+        if (err) {
+          res.send({
+            result: "something went wrong , Please try after sometime",
+          });
+        }
+        res.send({ user, auth: token });
+      });
     } else {
       res.send("No User Found");
     }
@@ -33,13 +51,14 @@ app.post("/login", async (req, res) => {
     res.send("both field are mandatory");
   }
 });
-
+//for add products
 app.post("/add-products", async (req, res) => {
   let product = new Products(req.body);
   let result = await product.save();
   res.send(result);
   console.log(result);
 });
+//for fetching list
 app.get("/list-products", async (req, res) => {
   let product = await Products.find();
   if (product.length > 0) {
@@ -48,19 +67,13 @@ app.get("/list-products", async (req, res) => {
     res.send({ result: "no product found !" });
   }
 });
-app.get("/list-products", async (req, res) => {
-  let product = await Products.find();
-  if (product.length > 0) {
-    res.send(product);
-  } else {
-    res.send({ result: "no product found !" });
-  }
-});
+// for delete  a specific product
 app.delete("/product/:id", async (req, res) => {
   let result = await Products.deleteOne({ _id: req.params.id });
   res.send(result);
   console.log(result);
 });
+//  for fetch one product  with the help of params
 app.get("/product/:id", async (req, res) => {
   let result = await Products.findOne({ _id: req.params.id });
   if (result) {
@@ -69,5 +82,35 @@ app.get("/product/:id", async (req, res) => {
     res.send({ result: "No record Found" });
   }
   // console.log(result);
+});
+
+// for update the product data
+app.put("/product/:id", async (req, res) => {
+  let result = await Products.updateOne(
+    { _id: req.params.id },
+    {
+      $set: req.body,
+    }
+  );
+  if (result) {
+    res.send(result);
+  } else {
+    res.send({ result: "No record Found" });
+  }
+  // console.log(result);
+});
+app.get("/search/:key", async (req, res) => {
+  let result = await Products.find({
+    $or: [
+      { name: { $regex: req.params.key } },
+      { category: { $regex: req.params.key } },
+      { compony: { $regex: req.params.key } },
+    ],
+  });
+  if (result) {
+    res.send(result);
+  } else {
+    res.send({ result: "No record Found" });
+  }
 });
 app.listen(5000);
